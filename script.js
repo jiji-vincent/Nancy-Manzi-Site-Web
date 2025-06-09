b6c4bbe7dcfdddf5892284731057e04fda415605
Jiji Vincent <jiji.vincent@icloud.com>
2025-06-09 01:43:59 -0400

    version 1.0

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
-// Hamburger Menu
+// HAMBURGER MENU
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
+}
+
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider() {
+
+  if(slides.length > 0){
+
+   slides[slideIndex].classList.add("displaySlide");
+   intervalId = setInterval(nextSlide, 5000);
+  }
+}
+
+function showSlide(index) {
+
+  if(index >= slides.length){
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach(slide => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide() {
+  clearInterval(intervalId)
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
 }
